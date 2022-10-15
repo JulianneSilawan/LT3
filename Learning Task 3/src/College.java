3830a4ba8eb56469410d826d6c5c39e0ee069db1
JulianneSilawan <100115102+JulianneSilawan@users.noreply.github.com>
2022-10-15 15:15:00 +0800

    Add files via upload

---
+++ Learning Task 3/src/College.java
@@ -0,0 +1,8 @@
+class College{
+    public void InformationTechnology (){
+        System.out.println("Object Oriented Programming");
+    }
+    public void USLS () {
+        System.out.println("ITP_203");
+    }
+}
